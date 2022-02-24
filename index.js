6bf9f3e1a44a1989c459e425e4f561b9292518bc
NoahSolis <NSolis1993@gmail.com>
2022-02-24 17:52:57 -0600

    commit tree ba4055da16dc816ffdaccd9127ed814d3995d89f

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = 'el bob'
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toLocaleUpperCase(); 
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "jimbo";
+}
